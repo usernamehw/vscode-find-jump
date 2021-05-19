4881f9bd350743e0836b7fa91fd049b5d516d5cc
al <usernamehw@gmail.com>
2021-05-19 18:20:53 +0300

    💄

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-//@ts-check
+// @ts-check
 
 'use strict';
 
@@ -7,7 +7,7 @@ const path = require('path');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = (env, options) => {
-	/**@type {import('webpack').Configuration}*/
+	/** @type {import('webpack').Configuration}*/
 	const config = {
 		target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
@@ -15,15 +15,18 @@ module.exports = (env, options) => {
 		output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
 			path: path.resolve(__dirname, 'dist'),
 			filename: 'extension.js',
-			libraryTarget: "commonjs2",
-			devtoolModuleFilenameTemplate: "../[resource-path]",
+			libraryTarget: 'commonjs2',
+			devtoolModuleFilenameTemplate: '../[resource-path]',
 		},
 		devtool: 'source-map',
 		externals: {
-			vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+			vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
 		},
 		resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-			extensions: ['.ts', '.js']
+			extensions: ['.ts', '.js'],
+			alias: {
+				"src": path.resolve('./src')
+			}
 		},
 		module: {
 			rules: [{
@@ -31,29 +34,26 @@ module.exports = (env, options) => {
 				exclude: /node_modules/,
 				use: [{
 					loader: 'ts-loader',
-				}]
-			}]
+				}],
+			}],
 		},
 		plugins: [
 			new FriendlyErrorsWebpackPlugin(),
-		]
+		],
 	};
 
 	if (options.mode === 'production') {
 		// Prod
 	} else {
 		// Dev
-		config.plugins.push(new webpack.DefinePlugin({
-			__DEV: JSON.stringify(true)
-		}));
 		config.module.rules[0] = {
 			test: /\.ts$/,
 			exclude: /node_modules/,
 			use: [{
 				loader: 'ts-loader',
-			}]
-		}
+			}],
+		};
 	}
 
 	return config;
-}
+};
